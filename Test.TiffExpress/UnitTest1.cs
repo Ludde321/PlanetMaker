71103d7be6d28e8f841b4b3378cd451d044ec012
Ludvig Pedersen <ludde@superoffice.com>
2017-12-07 13:21:38 +0100

    first test

--- Test.TiffExpress/UnitTest1.cs
+++ Test.TiffExpress/UnitTest1.cs
@@ -1,5 +1,7 @@
 using System;
 using Xunit;
+using TiffExpress;
+using System.IO;
 
 namespace Test.TiffExpress
 {
@@ -8,7 +10,15 @@ namespace Test.TiffExpress
         [Fact]
         public void Test1()
         {
-
+            using(var tiffReader = new TiffReader(File.OpenRead(@"TestData\gray8.tif")))
+            {
+                var ifd = tiffReader.ImageFileDirectories[0];
+                Assert.Equal(ifd.ImageWidth, 64);
+                Assert.Equal(ifd.ImageHeight, 64);
+                Assert.Equal(ifd.BitsPerSample, 8);
+                Assert.Equal(ifd.SamplesPerPixel, 1);
+                var bitmap = tiffReader.ReadImageFile<byte>(ifd).ToBitmap();
+            }
         }
     }
 }
